3f0dd6759dbf8214666df2ca11fb21e79518442f
Duccio Rocchini <ducciorocchini@gmail.com>
2023-09-14 05:07:45 +0200

    Create 06.var.R

---
+++ Code/06.var.R
@@ -0,0 +1,27 @@
+# Calculation spatial variability on remote sensing data
+
+# R code variability
+
+library(imageRy)
+library(terra)
+library(viridis)
+
+im.list
+
+sent <- im.import("sentinel.png")
+
+# band1 = NIR
+# band2 = red
+# band3 = green
+
+im.plotRGB.user(sent, 1, 2, 3)
+
+# calculation of variability over NIR
+nir <- sent[[1]]
+
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
+
+viridis <- colorRampPalette(viridis(7))(255)
+plot(sd3, col=viridis)
+
+
