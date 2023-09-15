bdb2de9fc04ec4c3ecb8f95c822a94341515e8d4
Duccio Rocchini <ducciorocchini@gmail.com>
2023-09-15 06:27:29 +0200

    Update 03.multitemporal.analysis.R

--- Code/03.multitemporal.analysis.R
+++ Code/03.multitemporal.analysis.R
@@ -1,62 +1,95 @@
-# Multitemporal analysis of ice melt in Greenland
+ # Procedure for classifying remote sensing data
 
-library(imageRy)
 library(terra)
+library(imageRy)
+library(ggplot2)
+# install.packages("patchwork")
+library(patchwork)
 
-# listing the available data
 im.list()
 
-# import data
-g2000 <- im.import("greenland.2000.tif")
-g2005 <- im.import("greenland.2005.tif")
-g2010 <- im.import("greenland.2010.tif")
-g2015 <- im.import("greenland.2015.tif")
+# Import the data
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
 
-# plot the data altogether
-cl <- colorRampPalette(c('light blue','blue','dark blue'))(100) 
-par(mfrow=c(2,2))
-plot(g2000, col=cl)
-plot(g2005, col=cl)
-plot(g2010, col=cl)
-plot(g2015, col=cl)
+# Classification
+im.plotRGB.user(m1992, 1, 2, 3)
 
-# stack
-gmulti <- c(g2000, g2005, g2010, g2015)
-gmulti
-plot(gmulti, col=cl)
+# 1992
+# 1. training sets
+val1992 <- values(m1992)
+val1992
 
-# Exercise: plot a single layer .from the stack 
-dev.off() # exit strategy in graphics
-plot(gmulti[[1]], col=cl)
+# 2. k-means
+k1992 <- kmeans(val1992, centers=2)
+k1992
+k1992$cluster
 
-# maximum (lag) change in time
-gdif = gmulti[[4]] - gmulti[[1]]
-cldif <- colorRampPalette(c("red","pink","light blue","blue"))(100)
-plot(gdif, col=cldif) 
+# 3. land cover map
+m1992c <- setValues(m1992[[1]], k1992$cluster)
+m1992c
+plot(m1992c)
 
-gdifall = gmulti[[4]] - gmulti[[3]] - gmulti[[2]] - gmulti[[1]]
-plot(gdifall, col=cldif) 
+# classes of 1992:
+# class 1: forest
+# class 2: agriculture
 
-# RGB
-im.plotRGB.user(gmulti, 1, 3, 4)
-# red 2000
-# green 2010
-# blue 2015
+#2006
 
+im.plotRGB.user(m2006, 1, 2, 3)
 
+# 1992
+# 1. training sets
+val2006 <- values(m2006)
+val2006
 
-cldif <- colorRampPalette(c("blue","light blue","pink","red"))(100)
-plot(gdif, col=cldif) 
+# 2. k-means
+k2006 <- kmeans(val2006, centers=2)
+k2006
+k2006$cluster
 
+# 3. land cover map
+m2006c <- setValues(m2006[[1]], k2006$cluster)
+m2006c
+plot(m2006c)
 
+# classes of 2006:
+# class 1: agriculture
+# class 2: forest
 
+# final estimates
+# 1992
+freq1992 <- freq(m1992c)
+freq1992
 
+freq2006 <- freq(m2006c)
+freq2006
 
+# percentages
+tot1992 = 1500 * 1200
+perc1992 = freq1992 * 100 / tot1992
+perc1992
 
+# 1992: forest = 83.08%, agriculture = 16.91%
 
+tot2006 = 3000 * 2400
+perc2006 = freq2006 * 100 / tot2006
+perc2006
 
+# 2006: forest = 45.31%, agriculture = 54.69%
 
+# building the output table
+cover <- c("forest", "agriculture") 
+perc1992 <- c(83.08, 16.91)
+perc2006 <- c(45.31, 54.69)
 
+# final table
+p <- data.frame(cover, perc1992, perc2006)
+p
 
+# final plot
+p1 <- ggplot(p, aes(x=cover, y=perc1992, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p2 <- ggplot(p, aes(x=cover, y=perc2006, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p1+p2
 
 
