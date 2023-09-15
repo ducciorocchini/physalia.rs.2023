bb9bd4026bde15a456fd98e5a9a45ac18f38c23e
Duccio Rocchini <ducciorocchini@gmail.com>
2023-09-15 06:27:01 +0200

    Update 03.multitemporal.analysis.R

--- Code/03.multitemporal.analysis.R
+++ Code/03.multitemporal.analysis.R
@@ -1,41 +1,62 @@
-# Time series analysis
-# Greenland increase of temperature
-# Data and code from Emanuela Cosma
+# Multitemporal analysis of ice melt in Greenland
 
-library(terra)
 library(imageRy)
+library(terra)
 
-# refresh the imageRy package to get new fresh data
-im.refresh()
-
-# list 
+# listing the available data
 im.list()
 
+# import data
 g2000 <- im.import("greenland.2000.tif")
 g2005 <- im.import("greenland.2005.tif")
 g2010 <- im.import("greenland.2010.tif")
 g2015 <- im.import("greenland.2015.tif")
 
-# par
+# plot the data altogether
+cl <- colorRampPalette(c('light blue','blue','dark blue'))(100) 
 par(mfrow=c(2,2))
-plot(g2000)
-plot(g2005)
-plot(g2010)
-plot(g2015)
+plot(g2000, col=cl)
+plot(g2005, col=cl)
+plot(g2010, col=cl)
+plot(g2015, col=cl)
 
 # stack
 gmulti <- c(g2000, g2005, g2010, g2015)
 gmulti
+plot(gmulti, col=cl)
 
-cl <- colorRampPalette(c('light blue','blue','dark blue'))(100) 
-plot(gmulti,col=cl)
+# Exercise: plot a single layer .from the stack 
+dev.off() # exit strategy in graphics
+plot(gmulti[[1]], col=cl)
 
-plot(gmulti,col=cl)
+# maximum (lag) change in time
+gdif = gmulti[[4]] - gmulti[[1]]
+cldif <- colorRampPalette(c("red","pink","light blue","blue"))(100)
+plot(gdif, col=cldif) 
 
-# RGB space
-im.plotRGB.user(gmulti, 1, 2, 3)
+gdifall = gmulti[[4]] - gmulti[[3]] - gmulti[[2]] - gmulti[[1]]
+plot(gdifall, col=cldif) 
 
-# Difference of two images in time
-gdif = g2015 - g2000
+# RGB
+im.plotRGB.user(gmulti, 1, 3, 4)
+# red 2000
+# green 2010
+# blue 2015
+
+
+
 cldif <- colorRampPalette(c("blue","light blue","pink","red"))(100)
-plot(gdif, col=cldif)
+plot(gdif, col=cldif) 
+
+
+
+
+
+
+
+
+
+
+
+
+
